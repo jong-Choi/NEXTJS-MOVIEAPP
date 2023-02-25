24061a14342e42f3929dd1383a389d7296619d07
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-02-26 05:01:05 +0900

    폰트 사이즈 경량화

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -1,6 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 400;
+  src: url('/fonts/NotoSansKR-Regular.woff2') format("woff2"),
+  url('/fonts/NotoSansKR-Regular.otf') format('opentype');
+  font-display: swap;
+}
+@font-face {
+  font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 700;
+  src: url('/fonts/NotoSansKR-Bold.woff2') format("woff2"),
+  url('/fonts/NotoSansKR-Bold.otf') format('opentype');
+  font-display: swap;
+}
   html,
   body {
     font-family: "Noto Sans KR", sans-serif;
